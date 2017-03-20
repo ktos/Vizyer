22b3303ec9fd78012ccd86655c41a69de2ed3a8e
Marcin Badurowicz <m@badurowicz.net>
2017-03-20 23:38:36 +0100

    initial revision (Template10 Minimal)

---
+++ Vizyer/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using Vizyer.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace Vizyer.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
